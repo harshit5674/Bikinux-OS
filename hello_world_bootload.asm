b543478af8fd901c9873ef1ff08afd7b41f31c7f
harshit5674 <harshit@Harshits-MacBook-Air.local>
2022-05-27 16:59:08 +0530

    Added a hello world bootloader by tradition ;)

---
+++ hello_world_bootload.asm
@@ -0,0 +1,28 @@
+mov ah,0x0e
+mov al,'H'
+int 0x10
+mov al,'E'
+int 0x10
+mov al,'L'
+int 0x10
+int 0x10
+mov al,'O'
+int 0x10
+mov al,0x20
+int 0x10
+mov al,'W'
+int 0x10
+mov al,'O'
+int 0x10
+mov al,'R'
+int 0x10
+mov al,'L'
+int 0x10
+mov al,'D'
+int 0x10
+
+jmp $
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+
